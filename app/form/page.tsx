827e9a4bc26e64b8637b5b76f32cd948900a4217
Rujinopy <ruth3442@gmail.com>
2024-10-14 20:33:07 +0700

    add: html-to-canvas

--- app/form/page.tsx
+++ app/form/page.tsx
@@ -1,13 +1,13 @@
 'use client';
 
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import { useForm, FormProvider } from 'react-hook-form';
 import ToothComponent from '@/components/ToothComponent';
 import { Kodchasan } from 'next/font/google'
 import RecordingButton from '@/components/RecordingButton';
 import Periovectors from '@/components/Periovectors';
 import { Quadrants } from '@/components/utils/utils';
-
+import SaveAsImageButton from '@/components/SaveAsImageButton'; // Import the button
 
 const kodchasan = Kodchasan({ subsets: ['thai'], weight: ['200', '300', '400', '500', '600'] })
 
@@ -15,6 +15,7 @@ const ChartComponent: React.FC = () => {
     const [audioURL, setAudioURL] = useState<string | null>(null);
     const [transcription, setTranscription] = useState('');
     const [loading, setLoading] = useState(false);
+    const formRef = useRef(null); // Reference to the form container
 
     const methods = useForm();
     const { setValue } = methods;
@@ -25,17 +26,30 @@ const ChartComponent: React.FC = () => {
                 console.log(transcription);
                 const dataArray = JSON.parse(transcription);
 
+
+
                 // Check if the parsed data is an array
                 if (Array.isArray(dataArray)) {
                     dataArray.forEach((data) => {
+                        const quadrant = data.toothNumber.slice(0, 1)
                         // Conditionally set depth if it exists
                         if (data.depth) {
-                            setValue(`${data.toothNumber}.depth`, data.depth);
+                            if (quadrant === '1' || quadrant === '3') {
+                                setValue(`${data.toothNumber}.depth`, data.depth);
+                            }
+                            else {
+                                setValue(`${data.toothNumber}.depth`, data.depth.toReversed());
+                            }
                         }
 
                         // Conditionally set margin if it exists
                         if (data.margin) {
-                            setValue(`${data.toothNumber}.margin`, data.margin);
+                            if (quadrant === '1' || quadrant === '3') {
+                                setValue(`${data.toothNumber}.margin`, data.margin);
+                            }
+                            else {
+                                setValue(`${data.toothNumber}.margin`, data.margin.toReversed());
+                            }
                         }
 
                         // Conditionally set plaque if it exists
@@ -46,6 +60,11 @@ const ChartComponent: React.FC = () => {
                         if (data.bleeding) {
                             setValue(`${data.toothNumber}.bleeding`, data.bleeding);
                         }
+
+                        if (data.mobility) {
+                            setValue(`${data.toothNumber}.mobility`, data.mobility);
+                        }
+
                     });
                 } else {
                     console.error("Expected an array but received something else");
@@ -63,23 +82,22 @@ const ChartComponent: React.FC = () => {
 
     return (
         <div>
-            <div className='bg-yellow-300 min-h-[20vh] flex items-center justify-center'>
+            <div className='bg-yellow-300 min-h-[20vh] flex items-center justify-center' >
                 <h2 className={`${kodchasan.className} bg-yellow-300 font-extrabold text-center py-8 text-6xl z-40`}>Periocheese :D</h2>
             </div>
-            <div className='fixed top-10 left-4 flex'>
+            <div className='fixed top-10 left-4 flex z-50'>
                 <RecordingButton
                     setAudioURL={setAudioURL}
                     setTranscription={setTranscription}
                     setLoading={setLoading}
                     loading={loading} // Pass the loading state here
                 />
+                <SaveAsImageButton formRef={formRef}/>
             </div>
-            <FormProvider {...methods}>
-                <form onSubmit={methods.handleSubmit(onSubmit)} className='mx-auto justify-center items-center w-[1200px] h-[1600px] top-0 left-0'>
-
-                    <div className="relative w-full mx-auto ">
-                    
-                        <svg className='absolute z-10 left-0 top-0 w-[1200px] h-[1600px]'>
+            <FormProvider {...methods} >
+                <form ref={formRef} onSubmit={methods.handleSubmit(onSubmit)}  className='mx-auto justify-center items-center w-[1200px] h-[1600px] top-0 left-0'>
+                    <div className="relative w-full mx-auto " >
+                        <svg className='absolute z-10 left-0 top-0 w-[1200px] h-[1600px] ' >
                             {['18b', '17b', '16b', '15b', '14b', '13b', '12b', '11b'].map(tooth => (
                                 <Periovectors key={tooth} toothNumber={tooth} quadrant={Quadrants.Q1B} />
                             ))}
@@ -92,21 +110,33 @@ const ChartComponent: React.FC = () => {
                             {['28p', '27p', '26p', '25p', '24p', '23p', '22p', '21p'].map(tooth => (
                                 <Periovectors key={tooth} toothNumber={tooth} quadrant={Quadrants.Q2P} />
                             ))}
+                            {['38b', '37b', '36b', '35b', '34b', '33b', '32b', '31b'].map(tooth => (
+                                <Periovectors key={tooth} toothNumber={tooth} quadrant={Quadrants.Q3B} />
+                            ))}
+                            {['38l', '37l', '36l', '35l', '34l', '33l', '32l', '31l'].map(tooth => (
+                                <Periovectors key={tooth} toothNumber={tooth} quadrant={Quadrants.Q3L} />
+                            ))}
+                            {['41b', '42b', '43b', '44b', '45b', '46b', '47b', '48b'].map(tooth => (
+                                <Periovectors key={tooth} toothNumber={tooth} quadrant={Quadrants.Q4B} />
+                            ))}
+                            {['41l', '42l', '43l', '44l', '45l', '46l', '47l', '48l'].map(tooth => (
+                                <Periovectors key={tooth} toothNumber={tooth} quadrant={Quadrants.Q4L} />
+                            ))}
                         </svg>
-                        <div className="md:scale-100 transform scale-100 mx-auto"> {/* Adjust scale as needed */}
+                        <div  className="md:scale-100 transform scale-100 mx-auto"> {/* Adjust scale as needed */}
                             <div className="absolute w-[1200px] h-[1600px] z-20 top-0 left-0">
                                 <img
-                                    src={"https://www.periodontalchart-online.com/uk/svg/uk-svg_grids-01.svg"}
+                                    src={`/grid/uk-svg_grids-01.svg`}
                                     alt="grid"
                                     width="1200"
                                     height="1600"
-                                    className="w-[1200px] h-[1600px]"
+                                    className="w-[1200px] h-[1600px]"                  
                                 />
 
                             </div>
                             <div className="absolute w-[1200px] h-[1600px] z-10 top-0 left-0">
                                 <img
-                                    src="https://www.periodontalchart-online.com/img/svg/svg_teeth.svg"
+                                    src={`/grid/svg_teeth.svg`}
                                     width="1200"
                                     height="1600"
                                     className='w-[1200px] h-[1600px]'
@@ -120,51 +150,60 @@ const ChartComponent: React.FC = () => {
 
                                 <div className='flex divide-x-2 divide-black border-2 border-black'>
                                     {['18b', '17b', '16b', '15b', '14b', '13b', '12b', '11b'].map(tooth => (
-                                        <ToothComponent key={tooth} toothNumber={tooth} quadrant='Q1B' />
+                                        <ToothComponent key={tooth} toothNumber={tooth} quadrant={Quadrants.Q1B} />
                                     ))}
                                 </div>
                                 <div className='flex divide-x-2 divide-black border-2 border-black'>
                                     {['21b', '22b', '23b', '24b', '25b', '26b', '27b', '28b'].map(tooth => (
-                                        <ToothComponent key={tooth} toothNumber={tooth} quadrant='Q2B' />
+                                        <ToothComponent key={tooth} toothNumber={tooth} quadrant={Quadrants.Q2B} />
                                     ))}
                                 </div>
                             </div>
                             <div className='flex space-x-10 bg-white absolute top-[28.2rem] -left-10 h-auto'>
                                 <div className='h-full bg-white absolute z-50 top-0 -left-20 w-[90px]'>
-                                        
                                 </div>
                                 <div className='h-full bg-white absolute z-50 top-0 -right-20 w-[80px]'> </div>
 
                                 <div className='flex divide-x-2 divide-black border-2 border-black'>
                                     {['18p', '17p', '16p', '15p', '14p', '13p', '12p', '11p'].map(tooth => (
-                                        <ToothComponent key={tooth} toothNumber={tooth} quadrant='Q1P' />
+                                        <ToothComponent key={tooth} toothNumber={tooth} quadrant={Quadrants.Q1P} />
                                     ))}
                                 </div>
                                 <div className='flex divide-x-2 divide-black border-2 border-black'>
                                     {['21p', '22p', '23p', '24p', '25p', '26p', '27p', '28p'].map(tooth => (
-                                        <ToothComponent key={tooth} toothNumber={tooth} quadrant='Q2P' />
+                                        <ToothComponent key={tooth} toothNumber={tooth} quadrant={Quadrants.Q2P} />
                                     ))}
                                 </div>
                             </div>
+                            <div className='flex space-x-8 bg-white absolute  top-[1065px] -left-10 h-auto' >
+                                <div className='h-full bg-white absolute z-50 top-0 -left-20 w-[90px]'></div>
+                                <div className='flex divide-x-2 divide-black border-2 border-black'>
+                                    {['48b', '47b', '46b', '45b', '44b', '43b', '42b', '41b'].map(tooth => (
+                                        <ToothComponent key={tooth} toothNumber={tooth} quadrant={Quadrants.Q4B} />
+                                    ))}
+                                </div>
+                                <div className='flex divide-x-2 divide-black border-2 border-black'>
+                                    {['31b', '32b', '33b', '34b', '35b', '36b', '37b', '38b'].map(tooth => (
+                                        <ToothComponent key={tooth} toothNumber={tooth} quadrant={Quadrants.Q3B} />
+                                    ))}
+                                </div>
 
-                            {/* Bottom Teeth */}
-                            {/* <div className='flex space-x-3'>
+                            </div>
+                            <div className='flex space-x-8 bg-white absolute top-[600px]' >
                                 <div className='flex divide-x-2 divide-black border-2 border-black'>
-                                    {['48', '47', '46', '45', '44', '43', '42', '41'].map(tooth => (
-                                        <ToothComponent key={tooth} toothNumber={tooth} />
+                                    {['48l', '47l', '46l', '45l', '44l', '43l', '42l', '41l'].map(tooth => (
+                                        <ToothComponent key={tooth} toothNumber={tooth} quadrant={Quadrants.Q4L} />
                                     ))}
                                 </div>
                                 <div className='flex divide-x-2 divide-black border-2 border-black'>
-                                    {['31', '32', '33', '34', '35', '36', '37', '38'].map(tooth => (
-                                        <ToothComponent key={tooth} toothNumber={tooth} />
+                                    {['31l', '32l', '33l', '34l', '35l', '36l', '37l', '38l'].map(tooth => (
+                                        <ToothComponent key={tooth} toothNumber={tooth} quadrant={Quadrants.Q3L} />
                                     ))}
                                 </div>
-                            </div> */}
+
+                            </div>
                         </div>
-
-
                     </div>
-
 
                     <button className='z-50 absolute bg-blue-500 text-white p-2 rounded-md bottom-4 right-4' type="submit">
                         Submit
