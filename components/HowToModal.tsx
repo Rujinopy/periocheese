60d9c4879d1158f67721f614dd6255593c854ea1
Rujinopy <ruth3442@gmail.com>
2025-06-02 22:06:22 +0700

    fix: wrap "how to use" button in a link to the how-to page

--- components/HowToModal.tsx
+++ components/HowToModal.tsx
@@ -8,9 +8,11 @@ export default function HowToModal() {
   return (
     <Dialog>
       <DialogTrigger asChild>
+        <a href="/how-to">
         <button className="p-2 bg-white hover:bg-gray-100 hover:cursor-pointer hover:underline rounded-full text-black">
           how to use
         </button>
+        </a>
       </DialogTrigger>
       <DialogContent className="max-w-[90vw] w-full max-h-[90vh] overflow-y-auto bg-green-100">
         <Howto />
